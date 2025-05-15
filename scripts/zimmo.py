deaf01ba5674679da0ae7d8e52dd69fac63b23f3
Dyluan <dylan.devoir@gmail.com>
2025-05-15 15:44:56 +0200

    added a gitignore file at root of project

--- scripts/zimmo.py
+++ scripts/zimmo.py
@@ -6,25 +6,20 @@ import time
 driver = webdriver.Firefox()
 driver.get("https://www.zimmo.be/fr/rechercher/?search=eyJmaWx0ZXIiOnsic3RhdHVzIjp7ImluIjpbIkZPUl9TQUxFIiwiVEFLRV9PVkVSIl19LCJwbGFjZUlkIjp7ImluIjpbNzJdfSwiY2F0ZWdvcnkiOnsiaW4iOlsiSE9VU0UiLCJBUEFSVE1FTlQiXX19fQ%3D%3D#gallery")
 
-# must accept cookies first
-areThereCookies = False
-accept_cookies_button = 'didomi-notice-agree-button'
+isThereACookiePopUp = True
 
-# needs to scroll down to access the accept all cookies button
-
-if (driver.find_element(By.ID, accept_cookies_button)):
-    areThereCookies = True
-    print('There are cookies to be accepted')
+if (driver.find_element(By.ID, 'didomi-popup')):
+    print('there is a popup page that wants us to accept the cookies')
+    accept_cookies_button = 'didomi-notice-agree-button'
     driver.find_element(By.ID, accept_cookies_button).click()
-print('no cookies to be accepted')
-
-time.sleep(2)
+    print('cookies accepted like a boss')
 
 # allows browser to wait until js elements are loaded
 time.sleep(3)
+
 big_container = driver.find_element(By.CLASS_NAME, 'property-results_container')
 print('big container found')
 nb_of_containers = driver.find_elements(By.CLASS_NAME, 'property_item')
-print(nb_of_containers)
+print(nb_of_containers, ' appartments/houses on the front page')
 
 driver.close()
