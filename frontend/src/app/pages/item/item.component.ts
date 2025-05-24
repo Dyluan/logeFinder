f6a3c2f8c3bc280216b10890bb57c4779b2989ef
Dyluan <dylan.devoir@gmail.com>
2025-05-24 18:30:43 +0200

    modified the carousel for a better styling and deleted the one I made myself

--- frontend/src/app/pages/item/item.component.ts
+++ frontend/src/app/pages/item/item.component.ts
@@ -2,13 +2,13 @@ import { Component, ViewChild, AfterViewInit, OnInit, inject } from '@angular/co
 import { HeaderComponent } from '../../components/header/header.component';
 import { MenuComponent } from '../../components/menu/menu.component';
 import { FooterComponent } from '../../components/footer/footer.component';
-import { ImageModalComponent } from '../../components/image-modal/image-modal.component';
 import { CommonModule } from '@angular/common';
 import { Appartement } from '../../models/appartement';
 import { AppartmentService } from '../../services/appartment.service';
 import { ActivatedRoute } from '@angular/router';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { environment } from '../../../environments/environment.development';
+import { GalleriaModule } from 'primeng/galleria';
 
 @Component({
   selector: 'app-item',
@@ -16,8 +16,8 @@ import { environment } from '../../../environments/environment.development';
     HeaderComponent,
     MenuComponent,
     FooterComponent,
-    ImageModalComponent,
-    CommonModule
+    CommonModule,
+    GalleriaModule,
   ],
   templateUrl: './item.component.html',
   styleUrl: './item.component.css'
@@ -36,41 +36,29 @@ export class ItemComponent implements OnInit {
   realUrl: SafeResourceUrl = '';
   googleMapsApi: string = environment.googleMapsApiKey;
 
+  galleriaImages: any[] = [];
+
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       let tempId = params.get('id');
       this.id = parseInt(tempId!);
     });
-    this.appartmentService.getAppartmentById(this.id!).subscribe(
-      data => {
+    this.appartmentService.getAppartmentById(this.id!).subscribe(data => {
         this.appartment = data;
         this.googleSrc = 'https://www.google.com/maps/embed/v1/place?key='+this.googleMapsApi+'&q='+this.appartment.adresse+','+this.appartment.ville;
         this.realUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.googleSrc);
         console.log('oninit' , this.appartment);
+
+        this.galleriaImages = (this.appartment.images ?? []).map(url => ({
+          itemImageSrc: url,
+          thumbnailImageSrc: url
+        }));
       }
     )
     
   }
 
-  @ViewChild('imageModal') imageModal!: ImageModalComponent;
-
-  //need to load those images from the server as well
-  imageUrls: string[] = [
-    'img/testImg.jpg',
-    'img/big.png',
-    'img/test2.jpg',
-    'img/small3.png',
-  ];
-
-  selectedImageIndex: number = 0;
-
   isMenuOpen = false;
-  isModalOpen = false;
-
-  //function related to the image modal
-  ngAfterViewInit(): void {
-      //
-  }
 
   onHeaderMenuClick() {
     this.isMenuOpen = !this.isMenuOpen;
@@ -79,18 +67,4 @@ export class ItemComponent implements OnInit {
     this.isMenuOpen = false;
   }
 
-  openImageModal(index: number) {
-    this.selectedImageIndex = index;
-    this.isModalOpen = true;
-    setTimeout(() => {
-      if (this.imageModal) {
-        this.imageModal.openModal(index);
-      }
-    });
-    
-  }
-
-  onModalClose() {
-    this.isModalOpen = false;
-  }
 }
