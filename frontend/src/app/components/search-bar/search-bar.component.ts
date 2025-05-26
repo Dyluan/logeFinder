b7e22e1e83a1789759db8fd618ed0481f3c4bf0f
Dyluan <dylan.devoir@gmail.com>
2025-05-26 14:37:29 +0200

    added a few filters to searchBar components using primeNG library.

--- frontend/src/app/components/search-bar/search-bar.component.ts
+++ frontend/src/app/components/search-bar/search-bar.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { SelectModule } from 'primeng/select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [],
+  imports: [
+    InputGroupModule,
+    InputGroupAddonModule,
+    SelectModule,
+    FormsModule
+  ],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
@@ -10,4 +19,16 @@ export class SearchBarComponent {
   onclick() {
     console.log('Search button clicked');
   }
+
+  selectedCity: any;
+  selectedType: any;
+  type_annonce = [
+    {name: 'Vente'},
+    {name: 'Location'}
+  ]
+  cities = [
+    {name: 'Auderghem'},
+    {name: 'Bruxelles'},
+    {name: 'Schaerbeek'}
+  ]
 }
