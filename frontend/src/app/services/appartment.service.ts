4f918985f050733ad9d63a779be685de049b7db3
Dyluan <dylan.devoir@gmail.com>
2025-05-26 12:01:14 +0200

    added a method to filters real estate goods to the appartments service

--- frontend/src/app/services/appartment.service.ts
+++ frontend/src/app/services/appartment.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { filter, Observable } from 'rxjs';
 import { Appartement } from '../models/appartement';
 
 @Injectable({
@@ -19,4 +19,37 @@ export class AppartmentService {
   getAppartmentById(id: number): Observable<Appartement> {
     return this.http.get<Appartement>(`${this.apiUrl}/${id}`);
   }
+
+  searchAppartments(filters: {
+    minPrice: number;
+    maxPrice?: number;
+    minSurface?: number;
+    city?: string;
+    minRooms?: number;
+    type_location?: string;
+    }): Observable<Appartement[]> {
+      let params = new HttpParams();
+      if (filters.minPrice) {
+        params = params.set('minPrice', filters.minPrice);
+      }
+      if (filters.maxPrice) {
+        params = params.set('maxPrice', filters.maxPrice);
+      }
+      if (filters.minSurface) {
+        params = params.set('minSurface', filters.minSurface)
+      }
+      if (filters.city) {
+        params = params.set('city', filters.city)
+      }
+      if (filters.minRooms) {
+        params = params.set('minRooms', filters.minRooms)
+      }
+      if (filters.type_location) {
+        params = params.set('type_location', filters.type_location)
+      }
+
+      return this.http.get<Appartement[]>(`${this.apiUrl}/search`, { params });
+  }
+
+
 }
